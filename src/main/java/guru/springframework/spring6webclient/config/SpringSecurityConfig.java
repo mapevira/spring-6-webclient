36b02b5fe91d559f19977df3587975e52df5f433
Raul Perez Vicente <r.perez.vicente@ibermatica.com>
2024-08-28 13:20:20 +0200

    335. Spring Security Configuration

--- src/main/java/guru/springframework/spring6webclient/config/SpringSecurityConfig.java
+++ src/main/java/guru/springframework/spring6webclient/config/SpringSecurityConfig.java
@@ -1,5 +1,10 @@
 package guru.springframework.spring6webclient.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.client.*;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
+
 /**
  * Created by jt, Spring Framework Guru.
  *
@@ -7,12 +12,6 @@ package guru.springframework.spring6webclient.config;
  * @version 28/08/2024 - 13:14
  * @since jdk 1.21
  */
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.oauth2.client.*;
-import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
-
 @Configuration
 public class SpringSecurityConfig {
 
